2aafb8ccbba251f358b9ca0bdc507f636a1d8b3d
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-12-07 10:25:06 +0000

    Added core `Shirt` logic

--- tshirt-project/src/canvas/components/Shirt.jsx
+++ tshirt-project/src/canvas/components/Shirt.jsx
@@ -15,7 +15,28 @@ const Shirt = () => {
   return (
     <group>
       <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
-        
+        {/* Check for full texture */}
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {/* Check for logo texture */}
+        {snap.isLogoTexture && (
+          <Decal 
+            position={[0, 0.04, 0.15]} 
+            rotation={[0, 0, 0]} 
+            scale={0.15} 
+            map={logoTexture}
+            anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
       </mesh>
     </group>
   )
